9ec42a067bec71678bce6de8f91e2adb14c9b099
benjamin <bkrauss18@icloud.com>
2026-03-27 11:09:47 +0100

    Initialisation Spring Boot et Hello World

---
+++ src/main/java/org/example/tp01/SpringDemoApplication.java
@@ -0,0 +1,20 @@
+package org.example.tp01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoApplication.class, args);
+    }
+
+    @GetMapping("hello")
+    public String hello() {
+        return "Bonjour, monde";
+    }
+}
